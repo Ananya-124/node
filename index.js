0b01814f16a03c0841dd74d69f50918b2d56a824
Ananya <ananyaananyaa124@gmail.com>
2024-10-26 09:35:49 +0530

    new

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const fs = require("fs");
-/*fs.writeFile("message.txt","HELLO FROM NODE.JS", (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  }); */
-  fs.readFile("./message.txt","utf-8",(err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }); 
+import inquirer from 'inquirer';
+import  qr from "qr-image"
+import fs from "fs"
+inquirer
+  .prompt([
+   { message:"type your url",
+     name:"URL"
+   }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url =answers.URL;
+    var qr_svg = qr.image(url);
+qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+ 
+fs.writeFile('URL.txt', url, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+}); 
+ 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
