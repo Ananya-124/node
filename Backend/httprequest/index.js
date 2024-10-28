3102122fb343e12e7187ad7efc02d58eafe09634
Ananya <ananyaananyaa124@gmail.com>
2024-10-28 20:55:08 +0530

    new

--- Backend/httprequest/index.js
+++ Backend/httprequest/index.js
@@ -1,6 +1,13 @@
 import express from "express"
 const app = express()
 const port = 3000
+app.get("/",(req,res) => {
+    console.log(req.rawHeaders)
+    res.send("<h1>Hello Welcome to my website</h1>")
+})
+app.get("/about",(req,res) => {
+    res.send("<h2>About Me</h2>")
+})
 app.listen(port,() =>{
     console.log("Server is running in port 3000")
 })
