55c2e70579a358d704233aecea7e548af4ca1793
Ananya <ananyaananyaa124@gmail.com>
2024-10-29 19:49:06 +0530

    new

--- Backend/httprequest/index.js
+++ Backend/httprequest/index.js
@@ -2,12 +2,20 @@ import express from "express"
 const app = express()
 const port = 3000
 app.get("/",(req,res) => {
-    console.log(req.rawHeaders)
     res.send("<h1>Hello Welcome to my website</h1>")
 })
-app.get("/about",(req,res) => {
-    res.send("<h2>About Me</h2>")
+app.post("/register",(req, res) =>{
+    res.sendStatus(201);
 })
-app.listen(port,() =>{
-    console.log("Server is running in port 3000")
+app.put("/user/ananya",(req,res) => {
+    res.sendStatus(200)
+})
+app.patch("/user/ananya",(req, res) => {
+    res.sendStatus(200)
+})
+app.delete("/user/ananya",(req,res) => {
+    res.sendStatus(200)
+})
+app.listen(port,() => {
+    console.log("server is running")
 })
